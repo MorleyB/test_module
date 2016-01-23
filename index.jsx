d1c90268604cbcaf06fabc6852ddfde1f6478c68
morleyb <mbankston@changehealthcare.com>
2016-01-22 21:29:42 -0600

    version 1.0.0 updates

---
+++ index.jsx
@@ -0,0 +1,12 @@
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>version: '1.0.0'</p>
+      </div>
+    );
+  }
+
+});
+
